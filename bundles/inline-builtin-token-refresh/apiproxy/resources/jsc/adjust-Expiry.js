432b3a988d670fe9d13ec6ec95c7c5ec316c5ee3
Dino Chiesa <dchiesa@google.com>
2020-04-13 09:43:15 -0700

    jshint cleanup

--- bundles/inline-builtin-token-refresh/apiproxy/resources/jsc/adjust-Expiry.js
+++ bundles/inline-builtin-token-refresh/apiproxy/resources/jsc/adjust-Expiry.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+
+/* global context, properties */
+
 var ttlValue = context.getVariable(properties.ttlValueVar);
 ttlValue -= parseInt(properties.delta, 10);
 context.setVariable(properties.ttlValueVar, ttlValue);
